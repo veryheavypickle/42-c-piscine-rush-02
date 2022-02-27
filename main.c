9070e640c6ecbcec76eb6858f8ed4cdc9fe36743
veryheavypickle <xavier.carroll01@gmail.com>
2022-02-27 23:42:11 +0100

    gywukhd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: xcarroll <xcarroll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 04:42:49 by xcarroll          #+#    #+#             */
-/*   Updated: 2022/02/27 22:49:59 by xcarroll         ###   ########.fr       */
+/*   Updated: 2022/02/27 23:40:42 by yzaytoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,41 @@ int	main(int argc, char **argv)
 		print_string("Error\n");
 }
 
-int	new_dict(char *new_key, char *to_convert)
+int	new_dict(char *key, char *new_value)
 {
+	int		count_1;
+	int		count_2;
+	char	*file_buffer;
+	char	*file;
+
+	count_1 = 0;
+	count_2 = 0;
+	file_buffer = (char*)malloc(sizeof(char) * 2000);
+	file = read_file("numbers.dict");
+	while (file[count_1] != '\0')
+	{
+		file_buffer[count_1] = new_values[count_1];
+		++count_1;
+	}
+	file_buffer[count_1] = '\n';
+	++count_1;
+	while (key[count_2] != '\0')
+	{
+		file_buffer[count_2] = key[count_2];
+		++count_2;
+	}
+	file_buffer[count_2] = ':';
+	++count_2;
+	file_buffer[count_2] = ' ';
+	++count_2;
+	while (new_values[count_2] != '\0')
+	{
+		file_buffer[count_2] = new_value[count_2];
+		++count_2;
+	}
+
 	print_string(to_convert);
 	print_string(new_key);
-	return (0);
+	free(file_buffer);
+	return(0);
 }
